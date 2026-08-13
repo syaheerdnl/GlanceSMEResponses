9609aa6cdd70744bb6a46674a780427ef7328628
Syaheer Daniel <muhammadsyaheerdaniel@gmail.com>
2026-08-13 19:15:31 +0800

    Publish full guest-mode SME prototype

--- prototype/flutter_bootstrap.js
+++ prototype/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4247580570" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "279435523" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
