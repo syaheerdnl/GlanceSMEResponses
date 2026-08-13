cd2193ca2951b7f34eeadabdcf080887300c24cc
syaheerdnl <muhammadsyaheerdaniel@gmail.com>
2026-08-13 10:37:07 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+//
+// Paste your Apps Script Web App URL here after deploying gas/Code.gs
+// (see gas/Code.gs's own header comment, and README.md, for the deploy
+// steps). It ends in /exec, e.g.:
+//   https://script.google.com/macros/s/AKfycb.../exec
+
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzuJx5xrsw0bq-qla_YaWnCrBagD4g1JL8f1HKFnlp7B2N1yI8MiWVbAkRtRuBfH5Ta/exec';
