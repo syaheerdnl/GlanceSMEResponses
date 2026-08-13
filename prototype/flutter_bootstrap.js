81a0dcd640ff8774d247ea9b79751d9a4de559b9
Syaheer Daniel <muhammadsyaheerdaniel@gmail.com>
2026-08-13 18:22:18 +0800

    Refresh study prototype build

--- prototype/flutter_bootstrap.js
+++ prototype/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3574495546" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3095334453" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
